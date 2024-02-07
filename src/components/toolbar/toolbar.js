a50c47d8fa12fa5ff0f0cc0d25da94a105436032
YS <k115502@naver.com>
2024-02-07 19:55:19 +0900

    feat: add route

--- src/components/toolbar/toolbar.js
+++ src/components/toolbar/toolbar.js
@@ -13,18 +13,16 @@ function Toolbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/activity">Activity</Nav.Link>
-            <Nav.Link href="/spa">Spa</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+            <Nav.Link href="/">Option</Nav.Link>
+            <NavDropdown title="SPA">
+              <NavDropdown.Item href='/spa1'>1</NavDropdown.Item>
+              <NavDropdown.Item href='/spa2'>2</NavDropdown.Item>
+              <NavDropdown.Item href='/spa3'>3</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="ACTIVITY">
+              <NavDropdown.Item href="/activity1">Activity1</NavDropdown.Item>
+              <NavDropdown.Item href="/activity2">Activity2</NavDropdown.Item>
+              <NavDropdown.Item href="/activity3">Activity3</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
